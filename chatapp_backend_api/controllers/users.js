94a9251d2905abc34bee2712d4847ee3f0fc63d7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-26 19:56:58 +0530

    Get All Users Method

--- chatapp_backend_api/controllers/users.js
+++ chatapp_backend_api/controllers/users.js
@@ -1,3 +1,17 @@
+const User = require ('../models/userModal');
+const httpStatus = require ('http-status-codes');
+
 module.exports = {
-  GetAllUsers (req, res) {},
+  async GetAllUsers (req, res) {
+    await User.find ({})
+      .populate ('posts.postId')
+      .then (result => {
+        res.status (httpStatus.OK).json ({message: 'All users', result});
+      })
+      .catch (err => {
+        res
+          .status (httpStatus.INTERNAL_SERVER_ERROR)
+          .json ({message: 'Error occured'});
+      });
+  },
 };
