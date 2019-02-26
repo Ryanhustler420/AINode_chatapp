c512234fecba41caf4961abffbc820d00749ca14
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-27 02:06:27 +0530

    Follow User Method

--- chatapp_backend_api/controllers/follows.js
+++ chatapp_backend_api/controllers/follows.js
@@ -1,3 +1,51 @@
+const HttpStatus = require ('http-status-codes');
+
+const User = require ('../models/userModal');
+
 module.exports = {
-  FollowUser (req, res) {},
+  FollowUser (req, res) {
+    const followUser = async () => {
+      // who is start following
+      await User.update (
+        {
+          _id: req.user._id,
+          'following.userFollowed': {$ne: req.body.userFollowed},
+        },
+        {
+          $push: {
+            following: {
+              userFollowed: req.body.userFollowed,
+            },
+          },
+        }
+      );
+
+      // who is followed
+      await User.update (
+        {
+          _id: req.body.userFollowed,
+          'followers.follower': {$ne: req.user._id},
+        },
+        {
+          $push: {
+            followers: {
+              follower: req.user._id,
+            },
+          },
+        }
+      );
+    };
+
+    followUser ()
+      .then (() => {
+        res.status (HttpStatus.OK).json ({
+          message: 'Following user now',
+        });
+      })
+      .catch (err => {
+        res.status (HttpStatus.INTERNAL_SERVER_ERROR).json ({
+          message: 'Error occured',
+        });
+      });
+  },
 };
