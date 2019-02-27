979f45899cb4160aeab0394cc3654a68fe684e3c
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-27 14:34:37 +0530

    Get Single User

--- chatapp_backend_api/routes/userRoutes.js
+++ chatapp_backend_api/routes/userRoutes.js
@@ -6,5 +6,7 @@ const UserCtrl = require ('../controllers/users');
 const Whois = require ('../Helpers/Whois');
 
 router.get ('/users', Whois.VerifyToken, UserCtrl.GetAllUsers);
+router.get ('/users/:id', Whois.VerifyToken, UserCtrl.GetUser);
+router.get ('/users/:username', Whois.VerifyToken, UserCtrl.GetUserByName);
 
 module.exports = router;
