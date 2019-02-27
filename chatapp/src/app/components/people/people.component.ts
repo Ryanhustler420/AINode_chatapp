4df7168d8808a3c6dd811f738ff28a679959a1fa
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-27 19:58:59 +0530

    Refresh Follow User Button

--- chatapp/src/app/components/people/people.component.ts
+++ chatapp/src/app/components/people/people.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
 import _ from 'lodash';
 import { TokenService } from 'src/app/services/token.service';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-people',
@@ -12,13 +13,21 @@ export class PeopleComponent implements OnInit {
   users = [];
   loggedInUser: any;
   userArray = [];
+  socket: any;
 
-  constructor(private userService: UsersService, private tokenService: TokenService) {}
+  constructor(private userService: UsersService, private tokenService: TokenService) {
+    this.socket = io('http://localhost:3000');
+  }
 
   ngOnInit() {
     this.loggedInUser = this.tokenService.GetPayloadOfToken();
-    this.GetUsers();
     this.GetUser();
+    this.GetUsers();
+
+    this.socket.on('refreshPage', () => {
+      this.GetUser();
+      this.GetUsers();
+    });
   }
 
   GetUsers() {
@@ -37,7 +46,8 @@ export class PeopleComponent implements OnInit {
 
   FollowUser(user) {
     this.userService.FollowUser(user).subscribe(data => {
-      console.log(data);
+      // console.log(data);
+      this.socket.emit('refresh', {});
     });
   }
 
