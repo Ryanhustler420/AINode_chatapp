91a849e0ba144263aa773491ced171ab0695b22a
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-27 20:43:45 +0530

    Display Followed User

--- chatapp/src/app/components/following/following.component.ts
+++ chatapp/src/app/components/following/following.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-following',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./following.component.scss']
 })
 export class FollowingComponent implements OnInit {
+  following = [];
+  user: any;
+  socket: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private tokenService: TokenService, private userService: UsersService) {
+    this.socket = io('http://localhost:3000');
   }
 
+  ngOnInit() {
+    this.user = this.tokenService.GetPayloadOfToken();
+    this.GetUser();
+
+    this.socket.on('refreshPage', () => {
+      this.GetUser();
+    });
+  }
+
+  GetUser() {
+    this.userService.GetUserById(this.user._id).subscribe(
+      data => {
+        // console.log(data);
+        this.following = data.result.following;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
