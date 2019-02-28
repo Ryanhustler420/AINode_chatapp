dfb79d93ddd04e30c43ecb8a7ef35d0ad29edaeb
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-28 15:26:50 +0530

    Get Notifications

--- chatapp/src/app/components/notifications/notifications.component.ts
+++ chatapp/src/app/components/notifications/notifications.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-notifications',
@@ -6,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
-  constructor() {}
+  loggedUser: any;
+  notifications = [];
 
-  ngOnInit() {}
+  socket: any;
+
+  constructor(private tokenService: TokenService, private usersService: UsersService) {
+    this.socket = io('http://localhost:3000');
+  }
+
+  ngOnInit() {
+    this.loggedUser = this.tokenService.GetPayloadOfToken();
+    this.GetUser();
+    this.socket.on('refreshPage', () => {
+      this.GetUser();
+    });
+  }
+
+  GetUser() {
+    this.usersService.GetUserById(this.loggedUser._id).subscribe(data => {
+      this.notifications = data.result.notifications;
+      console.log(this.notifications);
+    });
+  }
 }
