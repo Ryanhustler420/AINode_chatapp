b7780998b7bd7c42d68c1b80389827789bc11366
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-01 12:28:05 +0530

    Unread Notifications Count

--- chatapp/src/app/components/toolbar/toolbar.component.ts
+++ chatapp/src/app/components/toolbar/toolbar.component.ts
@@ -5,6 +5,7 @@ import * as M from 'materialize-css';
 import { UsersService } from 'src/app/services/users.service';
 import * as moment from 'moment';
 import io from 'socket.io-client';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-toolbar',
@@ -15,6 +16,7 @@ export class ToolbarComponent implements OnInit {
   user: any;
   notifications = [];
   socket: any;
+  unreadNotifications = [];
 
   constructor(private tokenService: TokenService, private router: Router, private userService: UsersService) {
     this.socket = io('http://localhost:3000');
@@ -52,6 +54,8 @@ export class ToolbarComponent implements OnInit {
     this.userService.GetUserById(this.user._id).subscribe(data => {
       this.notifications = data.result.notifications.reverse();
       // console.log(this.notifications);
+      const value = _.filter(this.notifications, ['read', false]);
+      this.unreadNotifications = value;
     });
   }
 
