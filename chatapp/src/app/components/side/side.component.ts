629f865596c34df5ecb3216e891ec9b4e8ab5fe8
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-01 16:58:05 +0530

    Side Component User Data

--- chatapp/src/app/components/side/side.component.ts
+++ chatapp/src/app/components/side/side.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-side',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side.component.scss']
 })
 export class SideComponent implements OnInit {
+  socket: any;
+  user: any;
+  metaInfo: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private tokenService: TokenService, private userService: UsersService) {
+    this.socket = io('http://localhost:3000');
   }
 
+  ngOnInit() {
+    this.user = this.tokenService.GetPayloadOfToken();
+    this.GetUser();
+    this.socket.on('refreshPage', () => {
+      this.GetUser();
+    });
+  }
+
+  GetUser() {
+    this.userService.GetUserById(this.user._id).subscribe(data => {
+      this.metaInfo = data.result;
+    });
+  }
 }
