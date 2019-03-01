027bbb9a0681660c5273e1f13b1ade11e9dc6a4d
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-01 21:38:23 +0530

    Fetching my posts from server

--- chatapp/src/app/components/my-post/my-post.component.ts
+++ chatapp/src/app/components/my-post/my-post.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import { TokenService } from 'src/app/services/token.service';
+import * as moment from 'moment';
+import _ from 'lodash';
+import { Router } from '@angular/router';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-my-post',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-post.component.scss']
 })
 export class MyPostComponent implements OnInit {
+  myPost = [];
+  user: any;
+  socket: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userService: UsersService, private tokenService: TokenService, private router: Router) {
+    this.socket = io('http://localhost:3000');
   }
 
+  ngOnInit() {
+    this.user = this.tokenService.GetPayloadOfToken();
+    this.GetUser();
+    this.socket.on('refreshPage', data => {
+      this.GetUser();
+    });
+  }
+
+  GetUser() {
+    this.userService.GetUserById(this.user._id).subscribe(data => {
+      this.myPost = data.result.posts;
+      // console.log(this.myPost);
+    });
+  }
+
+  CheckInLikesArray(arr, username) {
+    return _.some(arr, { username: username });
+  }
+
+  TimeFromNow(time) {
+    return moment(time).fromNow();
+  }
+
+  OpenCommentBox(post) {
+    this.router.navigate(['post', post._id]);
+  }
 }
