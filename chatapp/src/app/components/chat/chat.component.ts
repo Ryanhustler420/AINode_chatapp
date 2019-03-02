9d8ca61d6a4b7c7b26162d22ad51944fe9eeffde
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-02 13:30:07 +0530

    Hide Nav Content on Chat Component

--- chatapp/src/app/components/chat/chat.component.ts
+++ chatapp/src/app/components/chat/chat.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
+  tabElement: any;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.tabElement = document.querySelectorAll('.couldBeHide');
+  }
+
+  ngAfterViewInit() {
+    this.tabElement.forEach(element => {
+      element.style.display = 'none';
+    });
+  }
 }
