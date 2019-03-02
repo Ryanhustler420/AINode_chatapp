2a408499f44a5d061b4c0fb0458819cd7f70f441
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-02 22:53:13 +0530

    Route Params

--- chatapp/src/app/components/message/message.component.ts
+++ chatapp/src/app/components/message/message.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
 import { MessageService } from 'src/app/services/message.service';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-message',
@@ -8,9 +10,27 @@ import { MessageService } from 'src/app/services/message.service';
   styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
-  constructor(private tokenService: TokenService, messageService: MessageService) {}
+  receiverName: String;
 
-  ngOnInit() {}
+  constructor(
+    private tokenService: TokenService,
+    messageService: MessageService,
+    private route: ActivatedRoute,
+    private userService: UsersService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(usernameFromUrlAtTheAbove => {
+      this.receiverName = usernameFromUrlAtTheAbove.name;
+      this.GetUserByUsername(this.receiverName);
+    });
+  }
+
+  GetUserByUsername(username) {
+    this.userService.GetUserByName(username).subscribe(data => {
+      console.log(data);
+    });
+  }
 
   SendMessage() {}
 }
