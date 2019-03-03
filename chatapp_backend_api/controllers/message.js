a63a464eb207d9191ae1eb163283bdc479c2644a
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-03 11:06:39 +0530

    Conversation Find Method

--- chatapp_backend_api/controllers/message.js
+++ chatapp_backend_api/controllers/message.js
@@ -1,5 +1,33 @@
+const HtppStatus = require ('http-status-codes');
+
+const Message = require ('../models/messageModals');
+const Conversation = require ('../models/coversationModals');
+const User = require ('../models/userModal');
+
 module.exports = {
   SendMessage (req, res) {
-    console.log (req.body);
+    const {senderId, receiverId} = req.params;
+
+    Conversation.find (
+      {
+        $or: [
+          {
+            participant: {
+              $elemMatch: {senderId: senderId, receiverId: receiverId},
+            },
+          },
+          {
+            participant: {
+              $elemMatch: {senderId: receiverId, receiverId: senderId},
+            },
+          },
+        ],
+      },
+      async (err, result) => {
+        if (result.length > 0) {
+        } else {
+        }
+      }
+    );
   },
 };
