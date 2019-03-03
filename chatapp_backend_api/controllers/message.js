238d854ebe0126993a01ee6db3c397e93cc8df22
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-03 11:37:03 +0530

    Add New Conversation Chunk 1

--- chatapp_backend_api/controllers/message.js
+++ chatapp_backend_api/controllers/message.js
@@ -5,10 +5,10 @@ const Conversation = require ('../models/coversationModals');
 const User = require ('../models/userModal');
 
 module.exports = {
-  SendMessage (req, res) {
+  async SendMessage (req, res) {
     const {senderId, receiverId} = req.params;
 
-    Conversation.find (
+    await Conversation.find (
       {
         $or: [
           {
@@ -26,6 +26,30 @@ module.exports = {
       async (err, result) => {
         if (result.length > 0) {
         } else {
+          const newConversation = new Conversation ();
+          newConversation.participant.push ({
+            senderId: req.user._id,
+            receiverId: req.params.receiverId,
+          });
+
+          const saveConversation = await newConversation.save ();
+
+          // console.log (saveConversation);
+          /**
+           * { _id: 5c7b6e0909e6c15738efd58e, <--- _id conversation
+                participant:
+                [ { _id: 5c7b6e0909e6c15738efd58f,
+                    senderId: 5c76ee113b95a76c48c1bc29,
+                    receiverId: 5c76ee263b95a76c48c1bc2a } ],
+                __v: 0 
+              }
+           */
+
+          const newMessage = new Message ();
+          newMessage.conversationId = saveConversation._id; // <-- _id conversation
+          newMessage.sender = req.user.username;
+          newMessage.receiver = req.body.receiverName;
+          newMessage.message.push ({});
         }
       }
     );
