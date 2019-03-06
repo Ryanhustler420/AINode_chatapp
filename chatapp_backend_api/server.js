f602c78a09953e24e93969845c3988ce1bd27139
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-06 11:19:08 +0530

    Edit Server File

--- chatapp_backend_api/server.js
+++ chatapp_backend_api/server.js
@@ -8,28 +8,41 @@ const app = express ();
 
 app.use (cors ());
 
-const dbconfig = require ('./config/secret');
-
 const server = require ('http').createServer (app);
 const io = require ('socket.io').listen (server);
 
-app.use ((req, res, next) => {
-  res.header ('Access-Control-Allow-Origin', '*');
-  res.header ('Access-Control-Allow-Credentials', 'true');
-  res.header (
-    'Access-Control-Allow-Methods',
-    'GET',
-    'POST',
-    'DELETE',
-    'PUT',
-    'OPTIONS'
-  );
-  res.header (
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-TypeError, Accept, Authorization'
-  );
-  next ();
-});
+require ('./socket/streams') (io);
+require ('./socket/typing') (io);
+
+const dbconfig = require ('./config/secret');
+const auth = require ('./routes/authRoutes');
+const posts = require ('./routes/postRoutes');
+const users = require ('./routes/userRoutes');
+const follow = require ('./routes/followRoute');
+const message = require ('./routes/messageRoute');
+
+/**
+ * This Middleware is doing exactly the same thing which cors() middleware
+ * is doing. so there is no need to use this code 
+ */
+
+// app.use ((req, res, next) => {
+//   res.header ('Access-Control-Allow-Origin', '*');
+//   res.header ('Access-Control-Allow-Credentials', 'true');
+//   res.header (
+//     'Access-Control-Allow-Methods',
+//     'GET',
+//     'POST',
+//     'DELETE',
+//     'PUT',
+//     'OPTIONS'
+//   );
+//   res.header (
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-TypeError, Accept, Authorization'
+//   );
+//   next ();
+// });
 
 // app.use (express.json ());
 app.use (express.json ({limit: '50mb'}));
@@ -42,15 +55,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect (dbconfig.url, {
   useNewUrlParser: true,
 });
-
-require ('./socket/streams') (io);
-require ('./socket/typing') (io);
-
-const auth = require ('./routes/authRoutes');
-const posts = require ('./routes/postRoutes');
-const users = require ('./routes/userRoutes');
-const follow = require ('./routes/followRoute');
-const message = require ('./routes/messageRoute');
 
 app.use ('/api/chatapp/v1/', auth);
 app.use ('/api/chatapp/v1/', posts);
