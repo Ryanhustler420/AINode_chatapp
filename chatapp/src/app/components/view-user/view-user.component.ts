460d5f06a73459983eb3ae587cf115f0ba14a4b0
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-11 17:14:25 +0530

    View User Tabs

--- chatapp/src/app/components/view-user/view-user.component.ts
+++ chatapp/src/app/components/view-user/view-user.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
   styleUrls: ['./view-user.component.scss']
 })
-export class ViewUserComponent implements OnInit {
+export class ViewUserComponent implements OnInit, AfterViewInit {
+  tabEl: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.tabEl = document.querySelectorAll('.couldBeHide');
+  }
+
+  ngAfterViewInit() {
+    this.tabEl.forEach(element => {
+      element.style.display = 'none';
+    });
   }
 
 }
