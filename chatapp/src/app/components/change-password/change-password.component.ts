508b010e4195d5646ba6bb5f8921d9b334827813
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-22 19:16:05 +0530

    Custom Password Match Validation

--- chatapp/src/app/components/change-password/change-password.component.ts
+++ chatapp/src/app/components/change-password/change-password.component.ts
@@ -21,11 +21,30 @@ export class ChangePasswordComponent implements OnInit {
       cpassword: ['', Validators.required],
       newPassword: ['', Validators.required],
       confirmPassword: ['', Validators.required]
-    })
+    }, {
+        validators: this.Validate.bind(this)
+      })
   }
 
   ChangePassword() {
 
+  }
+
+  Validate(passwordFormGroup: FormGroup) {
+    const new_password = passwordFormGroup.controls.newPassword.value;
+    const confirm_password = passwordFormGroup.controls.confirmPassword.value;
+
+    if (confirm_password.length <= 0) {
+      return null;
+    }
+
+    if (confirm_password !== new_password) {
+      return {
+        doesNotMatch: true
+      }
+    }
+
+    return null;
   }
 
 }
