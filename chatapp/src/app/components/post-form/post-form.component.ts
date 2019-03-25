1a49c56e151082be3f5712edd88cf01918962dcf
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-25 10:10:59 +0530

    Send Image With Post FE

--- chatapp/src/app/components/post-form/post-form.component.ts
+++ chatapp/src/app/components/post-form/post-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PostService } from 'src/app/services/post.service';
-import io from 'socket.io-client';
+import * as io from 'socket.io-client';
+import { FileUploader } from 'ng2-file-upload';
+
+const URL = `http://localhost:3000/api/chatapp/v1/upload-image`;
 
 @Component({
   selector: 'app-post-form',
@@ -11,6 +14,13 @@ import io from 'socket.io-client';
 export class PostFormComponent implements OnInit {
   socket: any;
   postForm: FormGroup;
+
+  uploader: FileUploader = new FileUploader({
+    url: URL,
+    disableMultipart: true
+  });
+
+  selectedFile: any;
 
   constructor(private fb: FormBuilder, private postService: PostService) {
     this.socket = io('http://localhost:3000');
@@ -26,8 +36,19 @@ export class PostFormComponent implements OnInit {
     });
   }
 
-  submitPost() {
-    this.postService.addPost(this.postForm.value).subscribe(data => {
+  SubmitPost() {
+    let body;
+    if (!this.selectedFile) {
+      body = {
+        post: this.postForm.value.post
+      }
+    } else {
+      body = {
+        post: this.postForm.value.post,
+        image: this.selectedFile
+      }
+    }
+    this.postService.addPost(body).subscribe(data => {
       // console.log(data);
       this.socket.emit('refresh', {
         /** you can pass data as an object from this event emit */
@@ -35,5 +56,32 @@ export class PostFormComponent implements OnInit {
       this.postForm.reset();
     });
     // console.log(this.postForm.value);
+  }
+
+  OnFileSelected(event) {
+    const file: File = event[0];
+
+    this.ReadAsBase64(file)
+      .then(result => {
+        this.selectedFile = result;
+      })
+      .catch(err => console.log(err));
+  }
+
+  ReadAsBase64(file): Promise<any> {
+    const reader = new FileReader();
+    const fileValue = new Promise((resolve, reject) => {
+      reader.addEventListener('load', () => {
+        resolve(reader.result);
+      });
+
+      reader.addEventListener('error', err => {
+        reject(err);
+      });
+
+      reader.readAsDataURL(file);
+    });
+
+    return fileValue;
   }
 }
